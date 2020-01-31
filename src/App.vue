4f6ea3a90aea10d2e5e29f3e3bae452ca4e7cd67
Liz Davidson <lizdavidson45@gmail.com>
2020-01-30 22:12:19 -0500

    Initial vue app setup

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import Logo from "@/components/Logo.vue"
+
+export default {
+	name: "app",
+	components: {
+		Logo
+	}
+}
+</script>
+
+<template>
+	<div id="app">
+		<Logo alt="Maripro logo" width="500" />
+	</div>
+</template>
+
+<style>
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
