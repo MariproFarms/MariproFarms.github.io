cbc04e44c84e58b40e7118505e29a9007c6bb74e
Liz Davidson <lizdavidson45@gmail.com>
2020-02-04 22:56:28 -0500

    Update nav bar styles

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,21 +9,47 @@ export default {
 </script>
 
 <template>
-	<nav>
-		<Logo class="logo" width="190" height="150" />
-		<ul>
-			<li><router-link to="/about">About</router-link></li>
-			<li><router-link to="/contact">Contact</router-link></li>
+	<nav class="nav">
+		<Logo class="nav__logo" width="190" height="120" />
+		<ul class="nav__list">
+			<li>
+				<router-link class="nav__link" to="/about">
+					About
+				</router-link>
+			</li>
+			<li>
+				<router-link class="nav__link" to="/contact">
+					Contact
+				</router-link>
+			</li>
 		</ul>
 	</nav>
 </template>
 
 <style lang="scss" scoped>
-nav {
+.nav {
 	position: absolute;
-}
+	width: 100vw;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 
-.logo {
-	margin-left: 20px;
+	&__logo {
+		margin-left: 20px;
+	}
+
+	&__list {
+		display: flex;
+	}
+
+	&__link {
+		margin: 0 $spacer;
+		font-size: 1.5rem;
+		text-decoration: none;
+		text-transform: uppercase;
+		transform: translateY(50%);
+		color: $chineseWhite88;
+		letter-spacing: 2px;
+	}
 }
 </style>
