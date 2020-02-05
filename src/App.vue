0b30d00907f9d0b5f8b57f8bc30e3c172a3c15be
Liz Davidson <lizdavidson45@gmail.com>
2020-02-04 22:56:28 -0500

    Update nav bar styles

--- src/App.vue
+++ src/App.vue
@@ -25,11 +25,10 @@ body {
 }
 
 #app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
-	color: #2c3e50;
+	font-family: "Montserrat", sans-serif;
 	animation-name: appear;
 	animation-duration: 2s;
 }
