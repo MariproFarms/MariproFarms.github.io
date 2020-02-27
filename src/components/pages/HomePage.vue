e73234d35fd40b7eeb8fa4e473fcdd8a589c4c82
Liz Davidson <lizdavidson45@gmail.com>
2020-02-27 00:32:02 -0500

    Add block quote for placeholder content

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -14,22 +14,31 @@ export default {
 </script>
 
 <template>
-	<progressive-background
-		:src="backgroundImg"
-		:placeholder="backgroundPlaceholder"
-		:blur="20"
-		custom-class="background-img"
-	>
-		<div class="above-fold" slot="content">
-			<h1 class="header-text">Fresh. Sustainable. Local.</h1>
-		</div>
-	</progressive-background>
+	<div>
+		<progressive-background
+			:src="backgroundImg"
+			:placeholder="backgroundPlaceholder"
+			:blur="20"
+			custom-class="background-img"
+		>
+			<div class="above-fold" slot="content">
+				<h1 class="header-text">Fresh. Sustainable. Local.</h1>
+			</div>
+		</progressive-background>
+		<blockquote
+			class="quote"
+			cite="http://www.tutufoundationusa.org/2015/10/07/10-pieces-of-wisdom-from-desmond-tutu-on-his-birthday/"
+		>
+			"Do your little bit of good where you are; it’s those little bits of
+			good put together that overwhelm the world."
+			<span class="quote__byline">– Archbishop Desmond Tutu</span>
+		</blockquote>
+	</div>
 </template>
 
 <style lang="scss" scoped>
 .above-fold {
 	height: 100vh;
-	padding: 60px;
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -42,9 +51,28 @@ export default {
 	);
 }
 
+.background-img {
+	height: 100vh;
+}
+
 .header-text {
 	color: $white;
 	font-family: "Montserrat", sans-serif;
 	font-size: 3rem;
+}
+
+.quote {
+	font-size: 1.5rem;
+	line-height: 2rem;
+	font-style: oblique;
+	text-align: left;
+	width: 60vw;
+	margin: $spacer * 2 auto;
+
+	&__byline {
+		display: block;
+		text-align: right;
+		font-size: 1rem;
+	}
 }
 </style>
