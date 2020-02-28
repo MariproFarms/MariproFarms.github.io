f5659b32d008595df9977b986a2e10f7dfc64a18
Liz Davidson <lizdavidson45@gmail.com>
2020-02-27 23:28:35 -0500

    Add basic home page content and draft language

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,78 +1,55 @@
 <script>
-const backgroundPlaceholder = require("@/assets/fishcopy.jpg")
-const backgroundImg = require("@/assets/fish.jpg")
+import OurPhilosophy from "@/components/home/OurPhilosophy.vue";
+const backgroundPlaceholder = require("@/assets/fishcopy.jpg");
+const backgroundImg = require("@/assets/fish.jpg");
 
 export default {
-	name: "HomePage",
-	data() {
-		return {
-			backgroundPlaceholder,
-			backgroundImg
-		}
-	}
-}
+  name: "HomePage",
+  components: {
+    OurPhilosophy
+  },
+  data() {
+    return {
+      backgroundPlaceholder,
+      backgroundImg
+    };
+  }
+};
 </script>
 
 <template>
-	<div>
-		<progressive-background
-			:src="backgroundImg"
-			:placeholder="backgroundPlaceholder"
-			:blur="20"
-			custom-class="background-img"
-		>
-			<div class="above-fold" slot="content">
-				<h1 class="header-text">Fresh. Sustainable. Local.</h1>
-			</div>
-		</progressive-background>
-		<blockquote
-			class="quote"
-			cite="http://www.tutufoundationusa.org/2015/10/07/10-pieces-of-wisdom-from-desmond-tutu-on-his-birthday/"
-		>
-			"Do your little bit of good where you are; it’s those little bits of
-			good put together that overwhelm the world."
-			<span class="quote__byline">– Archbishop Desmond Tutu</span>
-		</blockquote>
-	</div>
+  <div>
+    <progressive-background
+      :src="backgroundImg"
+      :placeholder="backgroundPlaceholder"
+      :blur="20"
+      custom-class="background-img"
+    >
+      <div class="above-fold" slot="content">
+        <h1 class="header-text">Fresh. Sustainable. Local.</h1>
+      </div>
+    </progressive-background>
+    <OurPhilosophy />
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .above-fold {
-	height: 100vh;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	background: rgb(45, 111, 193);
-	background: linear-gradient(
-		4deg,
-		rgba(45, 111, 193, 0.45) 0%,
-		rgba(45, 111, 193, 0.45) 55%,
-		rgba(45, 111, 193, 0.45) 100%
-	);
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: $spacer;
+  background: rgba(45, 111, 193, 0.75);
 }
 
 .background-img {
-	height: 100vh;
+  height: 100vh;
 }
 
 .header-text {
-	color: $white;
-	font-family: "Montserrat", sans-serif;
-	font-size: 3rem;
-}
-
-.quote {
-	font-size: 1.5rem;
-	line-height: 2rem;
-	font-style: oblique;
-	text-align: left;
-	width: 60vw;
-	margin: $spacer * 2 auto;
-
-	&__byline {
-		display: block;
-		text-align: right;
-		font-size: 1rem;
-	}
+  color: $white;
+  font-family: "Montserrat", sans-serif;
+  font-size: 3rem;
 }
 </style>
