864221c6d324a859a7f94c7b5b59a3d582608b6f
Liz Davidson <lizdavidson45@gmail.com>
2020-06-24 18:51:31 -0400

    Fix flex bug on safari

--- src/components/home/ContentSection.vue
+++ src/components/home/ContentSection.vue
@@ -1,45 +1,46 @@
 <script>
 export default {
-  name: "ContentSection",
-  props: {
-    title: {
-      type: String,
-      required: false
-    },
-    text: {
-      type: String,
-      required: false
-    }
-  }
-};
+	name: "ContentSection",
+	props: {
+		title: {
+			type: String,
+			required: false
+		},
+		text: {
+			type: String,
+			required: false
+		}
+	}
+}
 </script>
 
 <template>
-  <div class="content__wrapper">
-    <div class="content">
-      <h2 v-if="title">{{title}}</h2>
-      <p>{{text}}</p>
-    </div>
-  </div>
+	<div class="content__wrapper">
+		<div class="content">
+			<h2 v-if="title">{{ title }}</h2>
+			<p>{{ text }}</p>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
 .content__wrapper {
-  display: flex;
-  flex-wrap: wrap;
-  width: 100vw;
-  align-content: center;
-  min-height: 100vh;
-  background: $ocean20;
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	width: 100vw;
+	justify-content: center;
+	min-height: 100vh;
+	background: $ocean20;
 }
 
 .content {
-  font-size: 1.6rem;
-  max-width: 900px;
-  text-align: left;
-  margin: $spacer * 2 auto;
-  padding: 0 $spacer;
-  line-height: 3rem;
-  color: #fff;
+	font-size: 1.6rem;
+	max-width: 900px;
+	text-align: left;
+	margin: $spacer * 2 auto;
+	padding: 0 $spacer;
+	line-height: 3rem;
+	color: #fff;
 }
 </style>
